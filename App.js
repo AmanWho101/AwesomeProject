f964197ea87b6da7a4c3a365aafbed3847178225
Amanwho101 <amleaz2005@gmail.com>
2023-05-30 17:06:36 +0300

    final30

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      profile: []
+    }
+  }
+  
+  var ApiFetch = { getData(){
+    var url = 'http://localhost:8000/v1/';
+    return fetch(url).then((res) => res.json);
+  }};
+  
+  componentDidMount(){
+    ApiFetch.getData.then((res));
+  }
+  
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hellow world!</Text>
+      <div>
+      ApiFetch
+      
+              who are you</div>
       <StatusBar style="auto" />
     </View>
   );
